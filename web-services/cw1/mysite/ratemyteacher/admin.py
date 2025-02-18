19f4a2671e422395d3c7de60d0fd19602a5a9ffd
sc22j4s <117829431+sc22j4s@users.noreply.github.com>
2025-02-18 23:55:00 +0000

    initial db schema done

--- web-services/cw1/mysite/ratemyteacher/admin.py
+++ web-services/cw1/mysite/ratemyteacher/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Professor, Module, Student
+from .models import Professor, Module, User, ModuleInstance, ProfessorModule, Rating
 
 # Register your models here.
-
 admin.site.register(Professor)
 admin.site.register(Module)
-admin.site.register(Student)
-
+admin.site.register(User)
+admin.site.register(Rating)
+admin.site.register(ModuleInstance)
+admin.site.register(ProfessorModule)
 
--- web-services/cw1/mysite/ratemyteacher/models.py
+++ web-services/cw1/mysite/ratemyteacher/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=4)
+    name = models.CharField(max_length=100)
+    rating = models.FloatField()
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+class Module(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=4)
+    name = models.CharField(max_length=100)
+
+class ModuleInstance(models.Model):
+    id = models.AutoField(primary_key=True)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    semester = models.IntegerField()
+
+class ProfessorModule(models.Model):
+    id = models.AutoField(primary_key=True)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    module_instance = models.ForeignKey(ModuleInstance, on_delete=models.CASCADE)   
+
+class Rating(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    professor_module = models.ForeignKey(ProfessorModule, on_delete=models.CASCADE)
+    rating = models.FloatField()
