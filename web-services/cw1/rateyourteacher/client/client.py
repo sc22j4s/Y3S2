dc23f4767181d11c28c642c69f13315af0fd9e81
sc22j4s <117829431+sc22j4s@users.noreply.github.com>
2025-02-20 15:58:39 +0000

    more client

--- web-services/cw1/rateyourteacher/client/client.py
+++ web-services/cw1/rateyourteacher/client/client.py
@@ -1,3 +1,45 @@
-import requests
+# import requests
 import json
+import os
 
+# URL = "sc22j4s@pythonanywhere.com"
+URL = "127.0.0.1"
+
+
+def register():
+    pass
+
+def help():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    help_file = os.path.join(base_dir, "help.txt")
+    try:
+        with open(help_file, "r") as f:
+            print(f.read())
+            f.close()   
+    except FileNotFoundError:
+            print("Help file not found")  
+
+
+
+def main():
+    print("Welcome to RateYourTeacher!\n")
+    print("Type 'help' for command list")
+
+    while True:
+        inp = input(">>> ")
+        
+        args = inp.split(" ")
+    
+        if args[0] == "help":
+            
+            help()
+        elif args[0] == "register":
+            pass
+
+
+    
+        else:
+            print(f"Command {inp} not recognised. Type \"help\" for command list.")
+
+if __name__ == "__main__": 
+    main()
